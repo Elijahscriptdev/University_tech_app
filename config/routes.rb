770d32bfb97205093e60504061e51c5ffbaff0d4
Elijahscriptdev <elijahobominuru@gmail.com>
2020-04-24 05:26:03 +0100

    add styling to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/home'
+  post 'signup', to: 'students#create'
+  get 'signup', to: 'students#new'
   get 'login', to: 'logins#new'
   post 'login', to: 'logins#create'
   delete 'logout', to: 'logins#destroy'
